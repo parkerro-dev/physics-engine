65c5b3512c2ef1cf29613f703814f6e7f92082b7
parkerro <parkerro.devs@gmail.com>
2022-08-17 18:33:14 +0100

    more foundational stuff

--- data/engine/scene/SceneManager.py
+++ data/engine/scene/SceneManager.py
@@ -7,10 +7,16 @@ from ...ui.uiMakers.TextMaker import TextMaker
 from .SceneBody import SceneBody
 
 class SceneManager():
-    def __init__(self, surface):
+    def __init__(self, hierarchySurface, console, engineSurface):
         self.SceneBodies = []
-        self.surface = surface
+        self.console = console
+        self.hierarchySurface = hierarchySurface
+        self.engineSurface = engineSurface
         self.textManager = TextMaker()
+        self.UIManager = None
+        self.menuActive = False
+        self.menuSurface = pygame.Surface((80, 100))
+        self.mousePos = None
 
     
     def addSceneBody(self, body, active = True):
@@ -22,13 +28,19 @@ class SceneManager():
 
     def hierarchyUI(self):
         # Outline
-        pygame.draw.rect(self.surface, (200,200,200), (3, 0, self.surface.get_width()-5, self.surface.get_height()-5), 2)
+        pygame.draw.rect(self.hierarchySurface, (200,200,200), (3, 0, self.hierarchySurface.get_width()-5, self.hierarchySurface.get_height()-5), 2)
 
         # title
-        self.textManager.makeText(self.surface, "Hierarchy", 5, 5)
+        self.textManager.makeText(self.hierarchySurface, "Hierarchy", 5, 5)
 
         # list bodies
 
+    def engineRMBMenu(self):
+        self.console.Log("We are making u a nice lil menu", self)
+        self.mousePos = pygame.mouse.get_pos()
+        self.UIManager.makeNewUIObject("engineMenu", self.menuSurface, ((self.mousePos), (self.mousePos[0]+80, self.mousePos[1]+100)), 2,)
+
+        self.menuActive = True
         
 
     def SceneLoop(self):
@@ -36,4 +48,8 @@ class SceneManager():
         for bodies in self.SceneBodies:
             bodies.body.makeBody()
             id = bodies.body.getID()
-            self.textManager.makeText(self.surface, "- Body {id}".format(id = id), 10, 5+(12*(id)))
+            self.textManager.makeText(self.engineSurface, "- Body {id}".format(id = id), 10, 5+(12*(id)))
+
+        if self.menuActive:
+            self.engineSurface.blit(self.menuSurface, self.mousePos)
+            self.menuSurface.fill((255, 0, 0))
